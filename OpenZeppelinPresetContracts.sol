16aed1da0327ff239c646f6f9d60f9665b6ce62a
Becky Button <EinsteinUnicorn@users.noreply.github.com>
2022-06-23 10:15:10 -0400

    Update OpenZeppelinPresetContracts.sol

--- OpenZeppelinPresetContracts.sol
+++ OpenZeppelinPresetContracts.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.7.4;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
